b5502d4aff19ebc3f6626e558df81bee6f518122
Pietro Rhyan <pietroqjg@gmail.com>
2022-07-24 00:54:48 -0300

    Adding miragejs and ReactQuery (with some bugs)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,75 +1,142 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react"
-import Link from "next/link"
-import { RiAddLine, RiPencilLine } from "react-icons/ri"
-import { Header } from "../../components/Header"
-import { Pagination } from "../../components/Pagination"
-import { Sidebar } from "../../components/Sidebar"
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { useEffect } from "react";
+import { RiAddLine } from "react-icons/ri";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+type User = {
+  id: string,
+  name: string,
+  email: string,
+  created_at: string,
+}
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery(["users"], async() => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        })
+      }
+    })
+
+    return users;
+  }, {
+    staleTime: 1000 * 5, // 5 seconds
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
-  })
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <Box>
       <Header />
 
-      <Flex
-        w='100%'
-        my='6'
-        maxWidth={1480}
-        mx='auto'
-        px='6'
-      >
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
-          <Flex mb='8' justify='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
-            <Link href='/users/create' passHref>
+            <Link href="/users/create" passHref>
               <Button
-                as='a'
-                size='sm'
-                fontSize='sm'
-                colorScheme='pink'
-                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
               >
                 Criar novo
               </Button>
             </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px='6' color='gray.300' w='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>Usuários</Th>
-                { isWideVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Pietro Rhyan</Text>
-                    <Text fontWeight='sm' color='gray.300'>pietroqjg@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>20 de Maio, 2022</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter os dados dos usuários!</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px="6" color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuários</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  { data.users.map((user: User) => {
+                    <Tr>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontWeight="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.created_at}</Td>}
+                    </Tr>;
+                  })}
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
