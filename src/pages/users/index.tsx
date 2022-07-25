9a46b904e8bd76ad3052216dc9b5affd1518fc99
Pietro Rhyan <pietroqjg@gmail.com>
2022-07-25 15:03:11 -0300

    Adding pagination system and useUsers hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,11 +18,10 @@ import {
 import Link from "next/link";
 import { RiAddLine } from "react-icons/ri";
 
-import { useQuery } from "@tanstack/react-query";
-
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../hooks/useUsers";
 
 type User = {
   id: string;
@@ -32,29 +31,7 @@ type User = {
 };
 
 export default function UserList() {
-  const { isLoading, error, data } = useQuery(["users"], async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
-
-      const users = data.users.map((user: User) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, // 5 seconds
-    }
-  );
+  const { isLoading, isFetching, error, data } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -72,6 +49,8 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+
+              { !isLoading && isFetching && <Spinner size='sm' color="gray.500" ml='4' />}
             </Heading>
 
             <Link href="/users/create" passHref>
